963cd2279ae6c3343d0a88e73a5aea0bc595bd00
jfm1894 <jfm1894@outlook.com>
2023-09-06 16:52:23 -0500

    some tables created

--- nutrition/models.py
+++ nutrition/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    userName = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    joinedOn = models.DateTimeField(auto_now_add=True)
+
+class FoodItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    barcode = models.CharField(max_length=50)
+    calories = models.IntegerField()
+    protein = models.IntegerField()
+    carbs = models.IntegerField()
+    fats = models.IntegerField()
+    unit = models.CharField(max_length=50)
+    servingSize = models.IntegerField()
+
+class Supplement(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    barcode = models.CharField(max_length=50)
+
+class Vitamin(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50)
