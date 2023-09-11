ab2f7e4687d5d73ed80f43f5f3d9c090ea9c5358
jfm1894 <jfm1894@outlook.com>
2023-09-11 17:05:33 -0500

    created models

--- nutrition/models.py
+++ nutrition/models.py
@@ -28,3 +28,59 @@ class Vitamin(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     unit = models.CharField(max_length=50)
+
+class Mineral(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50)
+
+class CombinedItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    userId = models.ForeignKey(Users, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+class Consumed(models.Model):
+    id = models.AutoField(primary_key=True)
+    userId = models.ForeignKey(Users, on_delete=models.CASCADE)
+    foodId = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    supplementId = models.ForeignKey(Supplement, on_delete=models.CASCADE)
+    combinedItemId = models.ForeignKey(CombinedItem, on_delete=models.CASCADE)
+    consumedAt = models.DateTimeField(auto_now_add=True)
+    portion = models.IntegerField()
+
+class CombinedFoodElement(models.Model):
+    id = models.AutoField(primary_key=True)
+    combinedFoodId = models.ForeignKey(CombinedItem, on_delete=models.CASCADE)
+    foodId = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    servingSize = models.IntegerField()
+
+class FoodVitamin(models.Model):
+    id = models.AutoField(primary_key=True)
+    foodId = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    vitaminId = models.ForeignKey(Vitamin, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+class FoodMineral(models.Model):
+    id = models.AutoField(primary_key=True)
+    foodId = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    mineralId = models.ForeignKey(Mineral, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+class SupplementVitamin(models.Model):
+    id = models.AutoField(primary_key=True)
+    supplementId = models.ForeignKey(Supplement, on_delete=models.CASCADE)
+    vitaminId = models.ForeignKey(Vitamin, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+class SupplementMineral(models.Model):
+    id = models.AutoField(primary_key=True)
+    supplementId = models.ForeignKey(Supplement, on_delete=models.CASCADE)
+    mineralId = models.ForeignKey(Mineral, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+class SupplementIngredient(models.Model):
+    id = models.AutoField(primary_key=True)
+    supplementId = models.ForeignKey(Supplement, on_delete=models.CASCADE)
+    foodId = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    unit = models.CharField(max_length=50)
