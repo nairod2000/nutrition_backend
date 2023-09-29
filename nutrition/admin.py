cbcaea109062766a2d6eba7a3bd6cb85036b2179
Casey Waters <waters1111@missouristate.edu>
2023-09-28 20:54:12 -0500

    Renamed some models and attributes

--- nutrition/admin.py
+++ nutrition/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import User, Unit, ServingSize, Nutrient, Item, CombinedItem, Consumed, CombinedItemElement, ItemNutrient, ItemBioactive, FavoriteItem, NutritionalGoalTemplate, GoalTemplateNutrient, UserNutritionalGoal, UserNutritionalGoalNutrient
+from .models import User, Unit, ServingSize, Nutrient, Item, CombinedItem, Consumed, CombinedItemElement, ItemNutrient, ItemBioactive, FavoriteItem, GoalTemplate, GoalTemplateNutrient, UserGoal, UserGoalNutrient
+
+
+class FavoriteItemInline(admin.TabularInline):
+    model = FavoriteItem
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'age', 'weight', 'height', 'sex', 'is_pregnant', 'is_staff', 'is_superuser', 'date_joined')
+    list_display = ('username', 'email', 'age', 'weight', 'height', 'sex', 'is_pregnant', 'is_staff', 'is_superuser', 'date_joined', 'favorites')
     list_filter = ('is_staff', 'is_superuser', 'date_joined')
+    inlines = [FavoriteItemInline]
     
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
@@ -12,74 +17,69 @@ class UnitAdmin(admin.ModelAdmin):
 
 @admin.register(ServingSize)
 class ServingSizeAdmin(admin.ModelAdmin):
-    list_display = ('amount', 'unitId')
+    list_display = ('amount', 'unit')
 
 @admin.register(Nutrient)
 class NutrientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'unitId', 'isCategory', 'parentNutrientId')
-
+    list_display = ('name', 'unit', 'isCategory', 'parentNutrient')
 
 class ItemNutrientInline(admin.TabularInline):
     model = ItemNutrient
-    extra = 1
 
 class ItemBioactiveInline(admin.TabularInline):
     model = ItemBioactive
-    extra = 1
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'barcode', 'calories', 'servingSizeId', 'userId', 'isCustom')
+    list_display = ('name', 'barcode', 'calories', 'servingSize', 'user', 'isCustom')
     list_filter = ('isCustom',)
     inlines = [ItemNutrientInline, ItemBioactiveInline]
     
 @admin.register(CombinedItem)
 class CombinedItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'userId')
+    list_display = ('name', 'user')
 
 @admin.register(Consumed)
 class ConsumedAdmin(admin.ModelAdmin):
-    list_display = ('userId', 'item_display', 'combined_item_display', 'consumedAt', 'portion')
-    list_filter = ('userId', 'consumedAt')
+    list_display = ('user', 'item_display', 'combined_item_display', 'consumedAt', 'portion')
+    list_filter = ('user', 'consumedAt')
 
     def item_display(self, obj):
-        return obj.itemId.name if obj.itemId else ''
+        return obj.item.name if obj.item else ''
     item_display.short_description = 'Item'
 
     def combined_item_display(self, obj):
-        return obj.combinedItemId.name if obj.combinedItemId else ''
+        return obj.combinedItem.name if obj.combinedItem else ''
     combined_item_display.short_description = 'Combined Item'
 
 @admin.register(CombinedItemElement)
 class CombinedItemElementAdmin(admin.ModelAdmin):
-    list_display = ('combinedItemId', 'itemId', 'portion')
+    list_display = ('combinedItem', 'item', 'portion')
 
 @admin.register(FavoriteItem)
 class FavoriteItemAdmin(admin.ModelAdmin):
-    list_display = ('userId', 'itemId')
+    list_display = ('user', 'item')
 
 class GoalTemplateNutrientInline(admin.TabularInline):
     model = GoalTemplateNutrient
-    extra = 1
 
-@admin.register(NutritionalGoalTemplate)
-class NutritionalGoalTemplateAdmin(admin.ModelAdmin):
+@admin.register(GoalTemplate)
+class GoalTemplateAdmin(admin.ModelAdmin):
     list_display = ('name', 'calories')
     inlines = [GoalTemplateNutrientInline]
 
 @admin.register(GoalTemplateNutrient)
 class GoalTemplateNutrientAdmin(admin.ModelAdmin):
-    list_display = ('templateId', 'nutrientId', 'recommendedValue')
+    list_display = ('template', 'nutrient', 'recommendedValue')
 
-class UserNutritionalGoalNutrientInline(admin.TabularInline):
-    model = UserNutritionalGoalNutrient
-    extra = 1
+class UserGoalNutrientInline(admin.TabularInline):
+    model = UserGoalNutrient
 
-@admin.register(UserNutritionalGoal)
-class UserNutritionalGoalAdmin(admin.ModelAdmin):
-    list_display = ('userId', 'name', 'templateId', 'calories')
-    inlines = [UserNutritionalGoalNutrientInline]
+@admin.register(UserGoal)
+class UserGoalAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'template', 'calories')
+    inlines = [UserGoalNutrientInline]
 
-@admin.register(UserNutritionalGoalNutrient)
-class UserNutritionalGoalNutrientAdmin(admin.ModelAdmin):
-    list_display = ('goalId', 'nutrientId', 'recommendedValue')
+@admin.register(UserGoalNutrient)
+class UserGoalNutrientAdmin(admin.ModelAdmin):
+    list_display = ('goal', 'nutrient', 'recommendedValue')
--- nutrition/models.py
+++ nutrition/models.py
@@ -30,17 +30,17 @@ class Unit(models.Model):
 class ServingSize(models.Model):
     # Represents a serving size, such as 1 pint or 100 grams.
     amount = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0.01)])
-    unitId = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.amount} {self.unitId}"
+        return f"{self.amount} {self.unit}"
 
 class Nutrient(models.Model):
     # Represents a nutrient, such as a macronutrient or a vitamin or mineral.
     name = models.CharField(max_length=50, unique=True)
-    unitId = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
     isCategory = models.BooleanField(default=False)
-    parentNutrientId = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    parentNutrient = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -50,15 +50,15 @@ class Item(models.Model):
     name = models.TextField()
     barcode = models.CharField(max_length=50, null=True, blank=True)
     calories = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0)])
-    servingSizeId = models.ForeignKey(ServingSize, on_delete=models.CASCADE)
+    servingSize = models.ForeignKey(ServingSize, on_delete=models.CASCADE)
     nutrients = models.ManyToManyField(Nutrient, through='ItemNutrient', related_name='items')
-    userId = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) # Creator of item (if applicable)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) # Creator of item (if applicable)
     favoritedBy = models.ManyToManyField(User, through='FavoriteItem', related_name='favorites')
     isCustom = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
         # If item is user-created, set isCustom to True.
-        if self.userId:
+        if self.user:
             self.isCustom = True
         super(Item, self).save(*args, **kwargs)
 
@@ -67,7 +67,7 @@ class Item(models.Model):
 
 class CombinedItem(models.Model):
     # Represents a user-defined combination of items, such as a recipe or a meal.
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -75,65 +75,65 @@ class CombinedItem(models.Model):
 
 class Consumed(models.Model):
     # Represents a user's consumption of an item or combined item.
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
-    itemId = models.ForeignKey(Item, on_delete=models.CASCADE, null=True, blank=True)
-    combinedItemId = models.ForeignKey(CombinedItem, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, null=True, blank=True)
+    combinedItem = models.ForeignKey(CombinedItem, on_delete=models.CASCADE, null=True, blank=True)
     consumedAt = models.DateTimeField(auto_now_add=True)
     portion = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0)])
 
     def clean(self):
-        # Ensure that at least one of itemId or combinedItemId is populated.
-        if not self.itemId and not self.combinedItemId:
-            raise ValidationError("At least one of itemId or combinedItemId must be populated.")
+        # Ensure that at least one of item or combinedItem is populated.
+        if not self.item and not self.combinedItem:
+            raise ValidationError("At least one of item or combinedItem must be populated.")
         
-        # Ensure that only one of itemId or combinedItemId is populated.
-        if self.itemId and self.combinedItemId:
-            raise ValidationError("Only one of itemId or combinedItemId should be populated.")
+        # Ensure that only one of item or combinedItem is populated.
+        if self.item and self.combinedItem:
+            raise ValidationError("Only one of item or combinedItem should be populated.")
         
     def __str__(self):
-        if self.itemId:
-            return f"Consumed: {self.itemId.name} by User: {self.userId.username}"
-        elif self.combinedItemId:
-            return f"Consumed: {self.combinedItemId.name} by User: {self.userId.username}"
+        if self.item:
+            return f"Consumed: {self.item.name} by User: {self.user.username}"
+        elif self.combinedItem:
+            return f"Consumed: {self.combinedItem.name} by User: {self.user.username}"
     
 class CombinedItemElement(models.Model):
     # Represents an item that is part of a combined item.
-    combinedItemId = models.ForeignKey(CombinedItem, on_delete=models.CASCADE)
-    itemId = models.ForeignKey(Item, on_delete=models.CASCADE)
+    combinedItem = models.ForeignKey(CombinedItem, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     portion = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0)])
 
     def __str__(self):
-        return f"Element: {self.itemId.name} in Combined Item: {self.combinedItemId.name}"
+        return f"Element: {self.item.name} in Combined Item: {self.combinedItem.name}"
 
 class ItemNutrient(models.Model):
     # Links an item to a nutrient and specifies the amount of that nutrient in the item.
-    itemId = models.ForeignKey(Item, on_delete=models.CASCADE)
-    nutrientId = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0)])
 
     def __str__(self):
-        return f"{self.itemId.name} - {self.nutrientId.name}"
+        return f"{self.item.name} - {self.nutrient.name}"
 
 
 class ItemBioactive(models.Model):
     # Represents a bioactive compound in an item.
-    itemId = models.ForeignKey(Item, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     amount = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0)])
-    unitId = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class FavoriteItem(models.Model):
     # Represents one of the favorite items of a user.
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
-    itemId = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.userId.username}'s favorite: {self.itemId.name}"
+        return f"{self.user.username}'s favorite: {self.item.name}"
 
-class NutritionalGoalTemplate(models.Model):
+class GoalTemplate(models.Model):
     # Represents a template for a nutritional goal, such as fda recommended daily values or weight loss or gain.
     name = models.CharField(max_length=50)
     calories = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0)])
@@ -144,35 +144,35 @@ class NutritionalGoalTemplate(models.Model):
     
 class GoalTemplateNutrient(models.Model):
     # Links a nutrient to a nutritional goal template and specifies the recommended value for that nutrient.
-    nutrientId = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
-    templateId = models.ForeignKey(NutritionalGoalTemplate, on_delete=models.CASCADE)
+    nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
+    template = models.ForeignKey(GoalTemplate, on_delete=models.CASCADE)
     recommendedValue = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0)])
 
     def __str__(self):
-        return f"{self.templateId.name} - {self.nutrientId.name}"
+        return f"{self.template.name} - {self.nutrient.name}"
     
-class UserNutritionalGoal(models.Model):
+class UserGoal(models.Model):
     # Represents a user's nutritional goals.
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100) # Name of goal, not user
-    templateId = models.ForeignKey(NutritionalGoalTemplate, on_delete=models.CASCADE)
+    template = models.ForeignKey(GoalTemplate, on_delete=models.CASCADE)
     calories = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0)])
-    nutrients = models.ManyToManyField(Nutrient, through='UserNutritionalGoalNutrient', related_name='goals')
+    nutrients = models.ManyToManyField(Nutrient, through='UserGoalNutrient', related_name='goals')
 
     def __str__(self):
-        return f"{self.userId.username}'s {self.name} Goal"
+        return f"{self.user.username}'s {self.name} Goal"
     
     def save(self, *args, **kwargs):
         # Set the name field based on the selected template's name
         if not self.name:
-            self.name = self.templateId.name
-        super(UserNutritionalGoal, self).save(*args, **kwargs)
+            self.name = self.template.name
+        super(UserGoal, self).save(*args, **kwargs)
     
-class UserNutritionalGoalNutrient(models.Model):
+class UserGoalNutrient(models.Model):
     # Links a nutrient to a user's nutritional goal and specifies the recommended value for that nutrient.
-    nutrientId = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
-    goalId = models.ForeignKey(UserNutritionalGoal, on_delete=models.CASCADE)
+    nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
+    goal = models.ForeignKey(UserGoal, on_delete=models.CASCADE)
     recommendedValue = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(0)])
 
     def __str__(self):
-        return f"{self.goalId.userId.username}'s {self.goalId.name} Goal - {self.nutrientId.name}"
+        return f"{self.goal.user.username}'s {self.goal.name} Goal - {self.nutrient.name}"
