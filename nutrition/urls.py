26ee14e7c82a48d0327179bda003c46ae4f9b471
jfm1894 <jfm1894@users.noreply.github.com>
2023-10-20 20:03:00 -0500

    will probably just delete this branch

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
+    path('', views.home_view, name='home'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('api/register', views.signup, name='api_register'),
 ]
--- nutrition/views.py
+++ nutrition/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Users
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signup_view(request):
@@ -6,3 +12,24 @@ def signup_view(request):
 
 def login_view(request):
     return render(request, 'login.html')
+
+def home_view(request):
+    return render(request, 'index.html')
+
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
+
+@api_view(['POST'])
+def signup(request):
+    if request.method == 'POST':
+        username = request.data['username']
+        password = request.data['password']
+        email = request.data['email']
+        try:
+            user = User.objects.create_user(username=username, password=password, email=email)
+            user.save()
+            return Response(status=status.HTTP_201_CREATED)
+        except Exception as e:
+            print(e)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
