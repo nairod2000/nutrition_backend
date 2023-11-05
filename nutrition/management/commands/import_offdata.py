3cef0aa002caf222983dac199c94386f9139d5b2
jfm1894 <jfm1894@users.noreply.github.com>
2023-11-05 12:10:53 -0600

    optimized importer and massaged data

---
+++ nutrition/management/commands/import_offdata.py
@@ -0,0 +1,97 @@
+import os
+import json
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from nutrition.models import Item, ServingSize, Nutrient, Unit, ItemNutrient
+import re
+from tqdm import tqdm
+from langdetect import detect
+
+
+
+
+class Command(BaseCommand):
+    help = 'Import item data from a JSONL file'
+
+    def handle(self, *args, **options):
+        # Define the relative path to CSV file
+        jsonl_file = os.path.normpath(os.path.join("data", "massagedData.jsonl"))
+        nutrients = []
+        serving_sizes = []
+        items = []
+        item_nutrients = []
+        unit_gram, created = Unit.objects.get_or_create(name='gram', abbreviation='g')
+        try:
+            # Open the CSV file and read its contents
+            with open(jsonl_file,'r',encoding='utf-8') as file:
+                for food_item in tqdm(file,total=246385):
+                    data = json.loads(food_item)
+                    if data.get('serving_size') >= 10**5:
+                        continue
+                    serving_size = ServingSize(amount=round(float(data.get('serving_size')),2), unit=unit_gram)
+                    serving_sizes.append(serving_size)
+                    item = Item(
+                        barcode=data.get('code'),
+                        name=data.get('product_name'),
+                        servingSize=serving_size,
+                        calories=data.get('calories')
+                    )
+                    items.append(item)
+                    for nutrient_name, nutrient_value in data.get('nutrients').items():
+                        nutrient, created = Nutrient.objects.get_or_create(
+                            name=nutrient_name,
+                            defaults={'unit': unit_gram}
+                        )
+                        nutrient_value = round(float(nutrient_value),2)
+                        if(nutrient_value >= 10**5):
+                            continue
+                        item_nutrient = ItemNutrient(
+                            item=item,  # This will be set after saving items
+                            nutrient=nutrient,
+                            amount=nutrient_value
+                        )
+                        item_nutrients.append(item_nutrient)
+            with transaction.atomic():
+                print('Writing serving sizes to database...')
+                # Bulk create all the serving sizes
+                ServingSize.objects.bulk_create(serving_sizes)
+                print('Done.')
+                print('Writing items to database...')
+                # Bulk create all the items
+                # Item.objects.bulk_create(items)
+                # print('Done.')
+                # print('Writing item nutrients to database...')
+                # # Bulk create all the item nutrients
+                # ItemNutrient.objects.bulk_create(item_nutrients)
+                # print('Done.')
+                # print('Updating item nutrient foreign keys...')
+                # # Update item nutrient foreign keys
+                # for item_nutrient in item_nutrients:
+                #     item_nutrient.item = item_nutrient.item
+                # ItemNutrient.objects.bulk_update(item_nutrients, ['item'])
+                # Bulk create all the items and save the returned objects
+                print('Creating items...')
+                created_items = Item.objects.bulk_create(items)
+                print('Done.')
+                # Create a mapping from the old items to the new items
+                print('Creating item mapping...')
+                item_mapping = {item: created_item for item, created_item in zip(items, created_items)}
+                print('Done.')
+                # Update the item field of each ItemNutrient object
+                print('Updating item nutrient foreign keys...')
+                for item_nutrient in item_nutrients:
+                    item_nutrient.item = item_mapping[item_nutrient.item]
+                print('Done.')
+                # Bulk create all the item nutrients
+                print('Creating item nutrients...')
+                ItemNutrient.objects.bulk_create(item_nutrients)
+                print('Done.')
+
+            self.stdout.write(self.style.SUCCESS('Successfully imported data.'))
+
+
+
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR('JSONL file not found. Please check the file path.'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'An error occurred: {e}'))
--- nutrition/management/commands/massagedata.py
+++ nutrition/management/commands/massagedata.py
@@ -24,22 +24,20 @@ def parse_float_from_string(input_string):
     else:
         raise ValueError("No leading number found in string")
 
-
-
 class Command(BaseCommand):
     help = 'Import item data from a JSONL file'
 
     def handle(self, *args, **options):
         # Define the relative path to CSV file
         jsonl_file = os.path.normpath(os.path.join("data", "openfoodfacts-products.jsonl"))
-        items = []
+        dataFile = open("massagedData.jsonl", "w")
+
         try:
             # Open the CSV file and read its contents
             with open(jsonl_file,'r',encoding='utf-8') as file:
                 for food_item in tqdm(file,total=2980745):
                     # Load the JSON data from each line
                     data = json.loads(food_item)
-                    #check that the product name is in english
                     product_name = data.get('product_name')
                     if product_name:
                         try:
@@ -51,44 +49,31 @@ class Command(BaseCommand):
                     # Skip items with missing data
                     if isAllZero(data.get('code')) or data.get('product_name') is None or data.get('product_name') == '' or data.get('nutriments') is None or data.get('serving_size') is None or data.get('nutriments').get('energy-kcal_100g') is None or data.get('serving_size').isalpha():
                         continue
-
-                    # Create a new unit object
                     try:
-                        unit_gram, created = Unit.objects.get_or_create(name='gram', abbreviation='g')
-                    except Unit.MultipleObjectsReturned:
-                        unit_gram = Unit.objects.filter(name='gram').first()
-
-                    #get nutrients
+                        serving_size_amount = parse_float_from_string(data.get('serving_size'))
+                    except ValueError:
+                        continue
+                    massaged_data = {}
+                    massaged_data['code'] = data.get('code')
+                    massaged_data['product_name'] = data.get('product_name')
                     nutrients = data.get('nutriments')
+                    nutrientsDict = {}
                     for key,value in nutrients.items():
                         if key.endswith("_serving"):
                             nutrient_name = key.replace("_serving", "")  # e.g., 'sugar'
                             amount_per_100g = value
-                            nutrient, created = Nutrient.objects.get_or_create(name=nutrient_name,
-                                                            defaults={'unit': unit_gram})
-                    #create a serving size
-                    try:
-                        serving_size_amount = parse_float_from_string(data.get('serving_size'))
-                    except ValueError:
-                        serving_size_amount = 1.0
-                    try:
-                        serving_size,created = ServingSize.objects.get_or_create(amount=serving_size_amount,unit=unit_gram)
-                    except:
-                        serving_size = ServingSize.objects.filter(amount=serving_size_amount,unit=unit_gram).first()
-                    # Create a new item object
-                    item, created = Item.objects.get_or_create(
-                        barcode=data.get('code'),
-                        defaults={
-                            'name': data.get('product_name'),
-                            'calories': data.get('nutriments').get('energy-kcal_100g'),
-                            'servingSize': serving_size,
-                        }
-                    )
-
-
-
+                            nutrientsDict[nutrient_name] = amount_per_100g
+                    massaged_data['nutrients'] = nutrientsDict
+                    massaged_data['calories'] = data.get('nutriments').get('energy-kcal_100g')
+                    massaged_data['serving_size'] = serving_size_amount
+                    dataFile.write(json.dumps(massaged_data))
+                    dataFile.write("\n")
 
         except FileNotFoundError:
             self.stdout.write(self.style.ERROR('JSONL file not found. Please check the file path.'))
         except Exception as e:
             self.stdout.write(self.style.ERROR(f'An error occurred: {e}'))
+        finally:
+            dataFile.close()
+
+    print("Done")
